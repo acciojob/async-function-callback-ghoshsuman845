ded9bbdf515a19d885e493413810358bcca57896
Suman Ghosh <ghoshsuman845@yahoo.com>
2023-03-17 17:23:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
+const btn = document.getElementById("btn");
 
 //your JS code here. If required.
+async function fetchData(showTitle) {
+const response = await fetch(url);
+const data = await response.json();
+showTitle(data.title);
+}
+
+function showMessage(message) {
+const output = document.getElementById("output");
+output.textContent = message;
+}
+
+btn.addEventListener("click", () => fetchData(showMessage));
